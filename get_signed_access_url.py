240d87075b8e211b89adda88a63851becd1f2078
Rahul G <rahul.gudise@gmail.com>
2023-06-13 13:56:57 -0400

    addded presigned url gen

---
+++ get_signed_access_url.py
@@ -0,0 +1,38 @@
+import os
+import logging
+import boto3
+from botocore.exceptions import ClientError
+
+def generate_url(bucket_name, object_name, expiration=3600):
+    """Generate a presigned URL to share an S3 object
+
+    :param bucket_name: string
+    :param object_name: string
+    :param expiration: Time in seconds for the presigned URL to remain valid
+    :return: Presigned URL as string. If error, returns None.
+    """
+
+    # Generate a presigned URL for the S3 object
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.generate_presigned_url('get_object',
+                                                    Params={'Bucket': bucket_name,
+                                                            'Key': object_name},
+                                                    ExpiresIn=expiration)
+    except ClientError as e:
+        logging.error(e)
+        return None
+
+    # The response contains the presigned URL
+    return response
+
+
+def get_presigned_access_url(object_name):
+    bucket = None   
+    try:
+        bucket = os.environ['UPLOAD_BUCKET']
+    except KeyError as e:
+        print(e)
+        return None
+    return generate_url(bucket, object_name, 3600)
+    
