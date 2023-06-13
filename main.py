66ae180a90d9a00d967aee727190d33d37da2967
Rishabh Sambare <rsambare@uwaterloo.ca>
2023-06-13 12:19:02 -0400

    created requirements.txt, added presigned_url for client-side bucket uploads, and ameliorated gitignore

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from typing import Union
-
-from fastapi import FastAPI
+import logging
+from fastapi import FastAPI, HTTPException
+from get_signed_url import get_upload_url
 
 app = FastAPI()
 
 @app.get("/hello")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/presigned")
+def presigned_url():
+    response = get_upload_url()
+    print("response", response)    
+    if response is None:
+        raise HTTPException(status_code=500, detail="Could not generate presigned url") 
+    return response 
