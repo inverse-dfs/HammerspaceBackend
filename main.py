bf682be58a0b42b8ac407e4a52672b699196a76e
Rishabh Sambare <rsambare@uwaterloo.ca>
2023-06-13 13:01:14 -0400

    quick cors bypass

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from typing import Union
 import logging
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
 from get_signed_url import get_upload_url
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/hello")
 def read_root():
