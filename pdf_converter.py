023fd077d92c849e124db08ae4db2d6f2061e3f1
Rahul G <rahul.gudise@gmail.com>
2023-06-30 10:03:34 -0400

    added pdf

--- pdf_converter.py
+++ pdf_converter.py
@@ -2,8 +2,9 @@ import subprocess
 from os.path import exists
 
 def convert_to_jpg(filename):
-    jpg_name = filename.rsplit('.', 1)[0] + 'jpg'
+    jpg_name = filename.rsplit('.', 1)[0]
     subprocess.run(["pdftoppm", "-jpeg", "-r", "300", filename, jpg_name])
     if not exists(jpg_name):
         jpg_name = filename.rsplit('.', 1)[0] + '-1.jpg'
+    subprocess.run["chmod 077 ", jpg_name]
     return jpg_name
