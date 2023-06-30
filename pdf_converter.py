5ae29a87a88dc0c5b8349a32565efa73bd30e766
Rahul G <rahul.gudise@gmail.com>
2023-06-30 10:12:53 -0400

    removed debug

--- pdf_converter.py
+++ pdf_converter.py
@@ -3,15 +3,6 @@ from os.path import exists
 
 def convert_to_jpg(filename):
     jpg_name = filename.rsplit('.', 1)[0]
-    print("")
-    print("")
-    print("")
-    print("")
-    print(jpg_name, filename)
-    print("")
-    print("")
-    print("")
-    print("")
     subprocess.run(["pdftoppm", "-jpeg", "-r", "300", filename, jpg_name])
     if not exists(jpg_name):
         jpg_name = filename.rsplit('.', 1)[0] + '-1.jpg'
